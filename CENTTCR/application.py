2f686ccace20b6bd9f3963bf9d0a5396f4986438
golfurier <golfurier@hotmail.com>
2018-05-06 00:35:26 +0700

    save query jinja

--- CENTTCR/application.py
+++ CENTTCR/application.py
@@ -4,63 +4,86 @@ from flask import Flask, render_template, request
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-
 app = Flask(__name__)
 
 engine = create_engine('postgresql://centcr:P@ssw0rd@localhost:5434/centcr')
 db = scoped_session(sessionmaker(bind=engine))
 
-#
+
 @app.route("/")
 def index():
-
-    sql = "SELECT * FROM tcr"
+    sql = "SELECT * FROM tcrs"
     tcrs = db.execute(sql).fetchall()
-    return render_template("index.html", tcrs = tcrs )
+    # for tcr in tcrs:
+    #     print(len(tcr))
+    #     print(tcr[i])
+    #     for i in range(len(tcr)):
+
+    return render_template("index.html", tcrs=tcrs)
+
+
+
+
 
 @app.route("/tcr")
 def tcr():
-    sql = """SELECT 
-                    tcr.case_num,
-                    tcr.mti,
-                    tcr.description,
-                    tcr.purpose,
-                    tcr.card,
-                    tcr.expected_values,
-                    tcr.actual_values,
+    sql = """SELECT tcrs.case_no,
+                    tcrs.mti,
+                    tcrs.case_name,
+                    tcrs.purpose,
+                    tcrs.card_no,
+                    tcrs.f4,
+                    tcrs.f39_exp,
+                    tcrs.f39_visa,
+                    tcrs.f39_mastercard,
+                    tcrs.f39_itmx,
+                    tcrs.f39_pcc,
+                    tcrs.f39_eps,
+                    tcrs.f39_clsp,
+                    tcrs.f39_clst,
+                    tcrs.f39_cardm,
+                    tcrs.f39_cbs,
+                    tcrs.seq_no,
+                    tcrs.tdate,
+                    tcrs.lgnt,
                     status.status,
-                    tcr.progress_comment,
+                    tcrs.progress_comment,
                     projects.proj_name,
                     switchings.sw_name,
                     transactions.trans_type,
                     actas.act_as,
                     testers.t_name,
                     accepters.a_name
-                FROM tcr
+                FROM tcrs
                 INNER 
                     JOIN status
-                    ON tcr.status_id = status.id
+                    ON tcrs.status_id = status.id
                 INNER
                     JOIN projects
-                    ON tcr.project_id = projects.id
+                    ON tcrs.project_id = projects.id
                 INNER
                     JOIN switchings
-                    ON tcr.switching_id = switchings.id
+                    ON tcrs.switching_id = switchings.id
                 INNER
                     JOIN transactions
-                    ON tcr.transaction_id = transactions.id
+                    ON tcrs.transaction_id = transactions.id
                 INNER
                     JOIN actas
-                    ON tcr.actas_Id = actas.id
+                    ON tcrs.actas_Id = actas.id
                 INNER 
                     JOIN testers
-                    ON tcr.tester_id = testers.id
+                    ON tcrs.tester_id = testers.id
                 INNER 
                     JOIN accepters
-                    ON tcr.accepter_id = accepters.id"""
+                    ON tcrs.accepter_id = accepters.id"""
     tcrs = db.execute(sql).fetchall()
+    for tcr in tcrs:
+        print(len(tcr))
+        for i in range(len(tcr)):
+            print(tcr[i])
     return render_template("tcr.html", tcr=tcrs)
 
 
+
 if __name__ == '__main__':
-   app.run()
+    app.run()
