4cb3bf87666e68ef8c355e6d4fc5cde274b21434
golfurier <golfurier@hotmail.com>
2018-05-05 20:18:52 +0700

    centcr dir uploaded

---
+++ centcr/sql.sql
@@ -0,0 +1,47 @@
+﻿SELECT 
+	tcr.case_num,
+	tcr.mti,
+	tcr.description,
+	tcr.purpose,
+	tcr.card,
+	tcr.expected_values,
+	tcr.actual_values,
+	status.status,
+	tcr.progress_comment,
+	projects.proj_name,
+	switchings.sw_name,
+	transactions.trans_type,
+	actas.act_as,
+	testers.t_name,
+	accepters.a_name
+FROM tcr
+INNER 
+	JOIN status
+	ON tcr.status_id = status.id
+INNER
+	JOIN projects
+	ON tcr.project_id = projects.id
+INNER
+	JOIN switchings
+	ON tcr.switching_id = switchings.id
+INNER
+	JOIN transactions
+	ON tcr.transaction_id = transactions.id
+INNER
+	JOIN actas
+	ON tcr.actas_Id = actas.id
+INNER 
+	JOIN testers
+	ON tcr.tester_id = testers.id
+INNER 
+	JOIN accepters
+	ON tcr.accepter_id = accepters.id
+
+
+
+
+
+
+
+
+	
